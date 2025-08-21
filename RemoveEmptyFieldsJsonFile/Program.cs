faf6396212d3b94189fd9b685ce2238d35f16eaa
Orlando Bustamante <bustamante30@gmail.com>
2025-08-21 16:13:43 -0500

    first commit

---
+++ RemoveEmptyFieldsJsonFile/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+
+class MainClass
+{
+    static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string s = await client.GetStringAsync("https://coderbyte.com/api/challenges/json/json-cleaning");
+        ExpandoObject expandoObject = JsonConvert.DeserializeObject<ExpandoObject>(
+            s, 
+            new ExpandoObjectConverter()
+        );
+        var expandoDict = (IDictionary<string, object>)expandoObject;
+        RemoveEmptyFields(expandoDict);
+        var result = JsonConvert.SerializeObject(expandoObject);
+
+        Console.WriteLine(result);
+    }
+
+    private static void RemoveEmptyFields(IDictionary<string, object>? expandoDict)
+    {
+        var innerDict = (IDictionary<string, object>)expandoDict;
+        var keysToRemove = new List<string>();
+        foreach (var innerKvp in innerDict)
+        {
+            if (innerKvp.Value is ExpandoObject innerEo)
+            {
+                RemoveEmptyFields(innerEo);
+            }
+            if (innerKvp.Value == null || (innerKvp.Value is string xx && string.IsNullOrEmpty(xx)))
+            {
+                keysToRemove.Add(innerKvp.Key);
+            }
+            if (innerKvp.Value is List<object> list)
+            {
+                ///added condition to remove "-" values, should be removed if not asked
+                list.RemoveAll(item => item == null || (item is string str && (string.IsNullOrEmpty(str)|| str == "-")));
+            }
+        }
+        foreach (var key in keysToRemove)
+        {
+            innerDict.Remove(key);
+        }
+    }
+}
